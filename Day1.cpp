1a9dac72d902a1e0d127bcd7b10a14a4b3c24acf
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-11-30 20:17:39 +0100

    Day0: Add boilerplate code
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

---
+++ Day1.cpp
@@ -0,0 +1,15 @@
+#include "Input.h"
+
+void Part1(const std::string& path) {
+
+std::vector<int> data = Input::GetData<int>(path, '\n');
+
+}
+
+void Part2(const std::string& path) {
+
+}
+
+int main() {
+
+}
