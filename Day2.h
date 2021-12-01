4c78e6f5724fbf6982dc43023e2e8a99dc345dca
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-01 20:01:34 +0100

    Day2: Boilerplate

---
+++ Day2.h
@@ -0,0 +1,16 @@
+#ifndef DAY_2
+#define DAY_2
+
+#include <vector>
+
+namespace aoc {
+namespace Day2 {
+int Part1(const std::vector<int>& data) {
+	return 0;
+}
+
+int Part2(const std::vector<int>& data) {
+	return 0;
+}
+}
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include "Day1.h"
+#include <chrono>
 
+#include "Day1.h"
+#include "Day2.h"
 #include "Input.h"
 
 const std::string dir{"PuzzleInput/"};
@@ -9,12 +11,44 @@ auto print = [](const std::string& name, const std::string& part1, const std::st
 };
 
 void Day1(std::vector<int> data) {
-	int part1 = aoc::Day1::Part1(data);
-	int part2 = aoc::Day1::Part2(data);
+	int part1, part2;
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		part1 = aoc::Day1::Part1(data);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
+	}
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		part2 = aoc::Day1::Part2(data);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
+	}
+	print(__func__, std::to_string(part1), std::to_string(part2));
+}
 
+void Day1(std::vector<int> data) {
+	int part1, part2;
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		part1 = aoc::Day2::Part1(data);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
+	}
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		part2 = aoc::Day2::Part2(data);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
+	}
 	print(__func__, std::to_string(part1), std::to_string(part2));
 }
 
 int main(int argc, const char** argv) {
 	Day1(Input::GetData<int>(dir + "Day1.txt", '\n'));
+	Day2(Input::GetData<int>(dir + "Day2.txt", '\n'));
 }
