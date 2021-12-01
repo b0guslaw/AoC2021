d0b4e3a559acf10578035b586bf3b6c9feb5b6f4
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-01 07:34:36 +0100

    Day1: Part 2 optimizaton

--- Day1.cpp
+++ Day1.cpp
@@ -2,28 +2,24 @@
 
 void Part1(const std::string& path) {
     std::vector<int> data = Input::GetData<int>(path, '\n');
-    int sum = 0;
     int last = data[0];
-    for (int i = 1; i < data.size(); i++) {
-        if (data[i] > last) {
-            ++sum;
-        }
-        last = data[i];
-    }
+    int sum = std::count_if(data.begin(), data.end(), [&](const int depth){
+        int res = depth > last;
+        last = depth;
+        return res;
+    });
+
     std::cout << sum << "\n";
 }
 
 void Part2(const std::string& path) {
     std::vector<int> data = Input::GetData<int>(path, '\n');
-
     int sum = 0;
-    int previousWindow = data[0] + data[1] + data[2];
-    for (int i = 1; i < data.size() - 2; i++) {
-        int newWindow = data[i] + data[i + 1] + data [i+2];
-        if (newWindow > previousWindow) {
+
+    for (int i = 0; i < data.size(); i++) {
+        if (data[i + 3] > data[i]) {
             ++sum;
         }
-        previousWindow = newWindow;
     }
     std::cout << sum << "\n";
 }
--- Input.h
+++ Input.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <algorithm>
+#include <numeric>
 
 namespace Input
 {
