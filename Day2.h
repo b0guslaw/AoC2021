d9718ddef82064b894eadea6ea55fee887debf75
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-02 07:43:57 +0100

    Day2: Optimization
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day2.h
+++ Day2.h
@@ -14,15 +14,15 @@ int Part1(const std::vector<std::string>& data) {
 	for (const auto& op : data) {
 		int amount = 0;
 		if (op.starts_with(forward)) {
-			amount = std::stoi(op.substr(forward.size(), op.size()));
+			amount = static_cast<int>(op[forward.size() + 1] - 48);
 			horiz += amount;
 		}
 		if (op.starts_with(up)) {
-			amount = std::stoi(op.substr(up.size(), op.size()));
+			amount = static_cast<int>(op[up.size() + 1] - 48);
 			depth -= amount;
 		}
 		if (op.starts_with(down)) {
-			amount = std::stoi(op.substr(down.size(), op.size()));
+			amount = static_cast<int>(op[down.size() + 1] - 48);
 			depth += amount;
 		}
 	}
@@ -35,16 +35,16 @@ int Part2(const std::vector<std::string>& data) {
 	for (const auto& op : data) {
 		int amount = 0;
 		if (op.starts_with(forward)) {
-			amount = std::stoi(op.substr(forward.size(), op.size()));
+			amount = static_cast<int>(op[forward.size() + 1] - 48);
 			horiz += amount;
 			depth += (aim * amount);
 		}
 		if (op.starts_with(up)) {
-			amount = std::stoi(op.substr(up.size(), op.size()));
+			amount = static_cast<int>(op[up.size() + 1] - 48);
 			aim -= amount;
 		}
 		if (op.starts_with(down)) {
-			amount = std::stoi(op.substr(down.size(), op.size()));
+			amount = static_cast<int>(op[down.size() + 1] - 48);
 			aim += amount;
 		}
 	}
