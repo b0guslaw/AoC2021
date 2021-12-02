4d3ed4376a432bfcc0349a8c24a904afd2f7f0d7
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-02 07:05:42 +0100

    Day2: Day 2 solution + general scaffolding fixes
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day2.h
+++ Day2.h
@@ -5,12 +5,50 @@
 
 namespace aoc {
 namespace Day2 {
-int Part1(const std::vector<int>& data) {
-	return 0;
+
+const std::string forward{"forward"}, up{"up"}, down{"down"};
+
+int Part1(const std::vector<std::string>& data) {
+	int horiz = 0, depth = 0;
+
+	for (const auto& op : data) {
+		int amount = 0;
+		if (op.starts_with(forward)) {
+			amount = std::stoi(op.substr(forward.size(), op.size()));
+			horiz += amount;
+		}
+		if (op.starts_with(up)) {
+			amount = std::stoi(op.substr(up.size(), op.size()));
+			depth -= amount;
+		}
+		if (op.starts_with(down)) {
+			amount = std::stoi(op.substr(down.size(), op.size()));
+			depth += amount;
+		}
+	}
+	return horiz * depth;
 }
 
-int Part2(const std::vector<int>& data) {
-	return 0;
+int Part2(const std::vector<std::string>& data) {
+	int horiz = 0, depth = 0, aim = 0;
+
+	for (const auto& op : data) {
+		int amount = 0;
+		if (op.starts_with(forward)) {
+			amount = std::stoi(op.substr(forward.size(), op.size()));
+			horiz += amount;
+			depth += (aim * amount);
+		}
+		if (op.starts_with(up)) {
+			amount = std::stoi(op.substr(up.size(), op.size()));
+			aim -= amount;
+		}
+		if (op.starts_with(down)) {
+			amount = std::stoi(op.substr(down.size(), op.size()));
+			aim += amount;
+		}
+	}
+	return horiz * depth;
 }
 }
 }
--- main.cpp
+++ main.cpp
@@ -8,49 +8,34 @@
 
 const std::string dir{"PuzzleInput/"};
 
-auto print = [](const std::string& name, const std::string& part1, const std::string& part2){
-	std::cout << "\t~~~ " << name << " ~~~" << "\nPart 1: " << part1 << "\nPart 2: " << part2 << "\n\n";
+auto print = [](const std::string& name, const std::string& part1, double elapsed1, const std::string& part2, double elapsed2){
+	std::cout << "\t~~~ " << name << " ~~~"
+				<< "\nPart 1: " << part1 << " elapsed time " <<	elapsed1 
+				<< "\nPart 2: " << part2 << " elapsed time " <<	elapsed2 << "\n\n";
 };
 
-void Day1(std::vector<int> data) {
-	int part1, part2;
-	{
+template<typename T>
+std::tuple<std::string, double> Runner(auto func, std::vector<T> input) {
 		auto start = std::chrono::high_resolution_clock::now();
-		part1 = aoc::Day1::Part1(data);
+		T result = func(input);
 		auto end = std::chrono::high_resolution_clock::now();
-		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
-	}
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		part2 = aoc::Day1::Part2(data);
-		auto end = std::chrono::high_resolution_clock::now();
-		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
-	}
-	print(__func__, std::to_string(part1), std::to_string(part2));
+		auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		return std::make_tuple(std::to_string(result), elapsed);
 }
 
-void Day2(std::vector<int> data) {
-	int part1, part2;
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		part1 = aoc::Day2::Part1(data);
-		auto end = std::chrono::high_resolution_clock::now();
-		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
-	}
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		part2 = aoc::Day2::Part2(data);
-		auto end = std::chrono::high_resolution_clock::now();
-		auto t = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << __func__ << ".1 Elapsed time " << t << "\n";
-	}
-	print(__func__, std::to_string(part1), std::to_string(part2));
+void Day1(std::vector<int> data) {
+	auto [result1, elapsed1] = Runner(aoc::Day1::Part1, data);
+	auto [result2, elapsed2] = Runner(aoc::Day1::Part2, data);
+	print(__func__, result1, elapsed1, result2, elapsed2);
+}
+
+void Day2(std::vector<std::string> data) {
+	auto [result1, elapsed1] = Runner(aoc::Day2::Part1, data);
+	auto [result2, elapsed2] = Runner(aoc::Day2::Part2, data);
+	print(__func__, result1, elapsed1, result2, elapsed2);
 }
 
 int main(int argc, const char** argv) {
 	Day1(Input::GetData<int>(dir + "Day1.txt", '\n'));
-	Day2(Input::GetData<int>(dir + "Day2.txt", '\n'));
+	Day2(Input::GetStringData(dir + "Day2.txt"));
 }
