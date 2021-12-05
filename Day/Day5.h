81a13fc1b4b5147ad21de9fb81eee0de6521f427
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-05 20:24:05 +0100

    General: Formatting

--- Day/Day5.h
+++ Day/Day5.h
@@ -11,7 +11,6 @@
 namespace aoc {
 namespace Day5 {
 
-//						x	, y
 using Point = std::pair<int, int>;
 
 void fill_field1(const std::vector<std::string>& input, std::map<Point, int>& field) {
@@ -39,41 +38,36 @@ void fill_field1(const std::vector<std::string>& input, std::map<Point, int>& fi
 void fill_field2(const std::vector<std::string>& input, std::map<Point, int>& field) {
 	int x1, y1, x2, y2;
 	char c_discard; std::string s_discard;
-for(const auto& line : input) {
-	std::stringstream iss(line);
-	iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
-	if (x1 == x2) {
-		for (int y{std::min(y1, y2)}; y <= std::max(y1, y2); y++) {
-			++field[{x1, y}];
-		}
-	} else if (y1 == y2) {
-		for (int x{std::min(x1, x2)}; x <= std::max(x1, x2); x++) {
-			++field[{x, y1}];
-		}
-	} else {
-		if (x1 < x2) {
-			if (y1 < y2) {
-				for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y++}];
-			}
-			if (y1 > y2) {
-				for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y--}];
-			}
-		}
-		if (x1 > x2) {
-			if (y1 < y2) {
-				for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y++}];
-			}
-			if (y1 > y2) {
-				for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y--}];
-			}
-		}
-	}
-}
-}
 
-void debug(std::map<Point, int> field) {
-	for (auto& [point, value] : field) {
-		std::cout << point.first << "," << point.second << " -> " << value << "\n";
+	for(const auto& line : input) {
+		std::stringstream iss(line);
+		iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
+		if (x1 == x2) {
+			for (int y{std::min(y1, y2)}; y <= std::max(y1, y2); y++) {
+				++field[{x1, y}];
+			}
+		} else if (y1 == y2) {
+			for (int x{std::min(x1, x2)}; x <= std::max(x1, x2); x++) {
+				++field[{x, y1}];
+			}
+		} else {
+			if (x1 < x2) {
+				if (y1 < y2) {
+					for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y++}];
+				}
+				if (y1 > y2) {
+					for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y--}];
+				}
+			}
+			if (x1 > x2) {
+				if (y1 < y2) {
+					for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y++}];
+				}
+				if (y1 > y2) {
+					for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y--}];
+				}
+			}
+		}
 	}
 }
 
