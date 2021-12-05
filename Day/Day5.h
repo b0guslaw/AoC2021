f16ac22c30d2b0be9487b505c42c3cb06625d10c
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-05 21:20:06 +0100

    Day5: Fix rebasing mistake
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day5.h
+++ Day/Day5.h
@@ -13,32 +13,30 @@ namespace Day5 {
 
 using Point = std::pair<int, int>;
 
-void fill_field1(const std::vector<std::string>& input, std::map<Point, int>& field) {
-	int x1, y1, x2, y2;
-	char c_discard; std::string s_discard;
-	for(const auto& line : input) {
-		std::stringstream iss(line);
-		iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
-		if (x1 > x2) { std::swap(x1, x2); std::swap(y1, y2); }
-		if (y1 > y2) { std::swap(x1, x2); std::swap(y1, y2); }
-		if (x1 == x2) {
-			for (int y{y1}; y <= y2; y++) {
-				++field[{x1, y}];
-			}
-		}
+using Point = std::pair<int, int>;
+void fill_field1(const std::vector<std::string> &input, std::map<Point, int>& field) {
+    int x1, y1, x2, y2;
+    char c_discard; std::string s_discard;
+    for(const auto& line : input) {
+        std::stringstream iss(line);
+        iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
+        if (x1 == x2) {
+            for (int y{std::min(y1, y2)}; y <= std::max(y1,y2); y++) {
+                ++field[{x1, y}];
+            }
+        }
 
-		if (y1 == y2) {
-			for (int x{x1}; x <= x2; x++) {
-				++field[{x, y1}];
-			}
-		}
-	}
+        if (y1 == y2) {
+            for (int x{std::min(x1, x2)}; x <= std::max(x1,x2); x++) {
+                ++field[{x, y1}];
+            }
+        }
+    }
 }
 
-void fill_field2(const std::vector<std::string>& input, std::map<Point, int>& field) {
-	int x1, y1, x2, y2;
-	char c_discard; std::string s_discard;
-
+void fill_field2(const std::vector<std::string> &input, std::map<Point, int>& field) {
+    int x1, y1, x2, y2;
+    char c_discard; std::string s_discard;
 	for(const auto& line : input) {
 		std::stringstream iss(line);
 		iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
