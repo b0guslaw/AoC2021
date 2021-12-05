b08e6392bfd7cbd628af83b613ce1865fb11799e
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-05 20:20:39 +0100

    Day5 finish
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day5.h
+++ Day/Day5.h
@@ -11,101 +11,92 @@
 namespace aoc {
 namespace Day5 {
 
-struct Point {
-	Point() : x{0}, y{0} {}
-	Point(int x, int  y) : x{x}, y{y} {}
-	Point(std::pair<int, int> pair) : x{pair.first}, y{pair.second} {}
-	int x, y;
+//						x	, y
+using Point = std::pair<int, int>;
 
-	bool operator==(const Point& other) const {
-		return x == other.x && y == other.y;
-	}
-
-	bool operator!=(const Point& other) const {
-		return !(*this == other);
-	}
-};
-
-struct Segment {
-	Segment(const Point& p1, const Point& p2) : p1{p1}, p2{p2} {
-		x_min = std::min(p1.x, p2.x);
-		x_max = std::max(p1.x, p2.x);
-		y_min = std::min(p1.y, p2.y);
-		y_max = std::max(p1.y, p2.y);
-	}
-	Point p1, p2;
-	int x_min, x_max, y_min, y_max;
-
-	bool operator==(const Segment& other) const {
-		return p1 == other.p1 && p2 == other.p2;
-	}
-
-	bool operator!=(const Segment& other) const {
-		return !(*this == other);
-	}
-
-	bool is_not_angled() const {
-		return p1.x == p2.x || p1.y == p2.y;
-	}
-};
-
-std::vector<int> parse_data(const std::vector<std::string> &input) {
-	std::vector<int> output;
+void fill_field1(const std::vector<std::string>& input, std::map<Point, int>& field) {
 	int x1, y1, x2, y2;
 	char c_discard; std::string s_discard;
-	for(auto line : input) {
+	for(const auto& line : input) {
 		std::stringstream iss(line);
 		iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
-		output.push_back(x1); output.push_back(y1);
-		output.push_back(x2); output.push_back(y2);
+		if (x1 > x2) { std::swap(x1, x2); std::swap(y1, y2); }
+		if (y1 > y2) { std::swap(x1, x2); std::swap(y1, y2); }
+		if (x1 == x2) {
+			for (int y{y1}; y <= y2; y++) {
+				++field[{x1, y}];
+			}
+		}
+
+		if (y1 == y2) {
+			for (int x{x1}; x <= x2; x++) {
+				++field[{x, y1}];
+			}
+		}
 	}
-	return output;
+}
+
+void fill_field2(const std::vector<std::string>& input, std::map<Point, int>& field) {
+	int x1, y1, x2, y2;
+	char c_discard; std::string s_discard;
+for(const auto& line : input) {
+	std::stringstream iss(line);
+	iss >> x1 >> c_discard >> y1 >> s_discard >> x2 >> c_discard >> y2;
+	if (x1 == x2) {
+		for (int y{std::min(y1, y2)}; y <= std::max(y1, y2); y++) {
+			++field[{x1, y}];
+		}
+	} else if (y1 == y2) {
+		for (int x{std::min(x1, x2)}; x <= std::max(x1, x2); x++) {
+			++field[{x, y1}];
+		}
+	} else {
+		if (x1 < x2) {
+			if (y1 < y2) {
+				for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y++}];
+			}
+			if (y1 > y2) {
+				for (int _x{x1}, _y{y1}; _x <= x2;) ++field[{_x++, _y--}];
+			}
+		}
+		if (x1 > x2) {
+			if (y1 < y2) {
+				for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y++}];
+			}
+			if (y1 > y2) {
+				for (int _x{x1}, _y{y1}; _x >= x2;) ++field[{_x--, _y--}];
+			}
+		}
+	}
+}
+}
+
+void debug(std::map<Point, int> field) {
+	for (auto& [point, value] : field) {
+		std::cout << point.first << "," << point.second << " -> " << value << "\n";
+	}
 }
 
 uint64_t Part1(const std::vector<std::string>& data) {
-	auto coordinates_raw = parse_data(data);
-	std::vector<Segment> segments;
+	std::map<Point, int> field;
+	fill_field1(data, field);
 
-	std::map<std::pair<int, int>, int> field;
-	for (int i{0}; i < coordinates_raw.size(); i += 4) {
-		Point p1(std::make_pair(
-			coordinates_raw.at(i), coordinates_raw.at(i + 1)
-		));
-		Point p2(std::make_pair(
-			coordinates_raw.at(i + 2), coordinates_raw.at(i + 3)
-		));
-
-		segments.emplace_back(p1, p2);
+	int overlaps = 0;
+	for (auto& [point, value] : field) {
+		if (value > 1) overlaps++;
 	}
-
-	for (const auto& s : segments) {
-
-		// std::cout << s.p1.x << "," << s.p1.y << " -> " << s.p2.x << "," << s.p2.y << "\n";
-		if (s.p1.x == s.p2.x) {
-			for(int i{s.y_min}; i <= s.y_max; i++) {
-				field[std::make_pair(s.p1.x, i)]++;
-			}
-		}
-		if (s.p1.y == s.p2.y) {
-			for(int i{s.x_min}; i <= s.x_max; i++) {
-				field[std::make_pair(i, s.p1.y)]++;
-			}
-		}
-	}
-
-	int overlaps{0};
-	for (auto const& p : field) {
-		//std::cout << p.first.first << "," << p.first.second << " -> " << p.second << "\n";
-		if (p.second > 1) {
-			++overlaps;
-		}
-	}
-
 	return static_cast<uint64_t>(overlaps);
 }
 
 uint64_t Part2(const std::vector<std::string>& data) {
-	return data.size();
+	std::map<Point, int> field;
+	fill_field2(data, field);
+
+	int overlaps = 0;
+	for (auto& [point, value] : field) {
+		if (value > 1) overlaps++;
+	}
+	return static_cast<uint64_t>(overlaps);
 }
 
 }
