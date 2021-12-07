857c36d53ea1cfca6efcffe4deea6d44c17cc776
Ralph <ralph.dworzanski@gmail.com>
2021-12-07 12:58:20 +0100

    Update Day7.h

--- Day/Day7.h
+++ Day/Day7.h
@@ -6,25 +6,26 @@
 #include <algorithm>
 #include <numeric>
 #include <array>
+#include <functional>
 
 namespace aoc {
 namespace Day7 {
 
 uint64_t Part1(const std::vector<int>& data) {
 	auto crabs = data;
-	std::sort(crabs.begin(), crabs.end());
-	int64_t pivot = crabs[crabs.size() / 2];
+	auto pivot = crabs.begin() + crabs.size() / 2;
+	std::nth_element(crabs.begin(), pivot, crabs.end(), std::less{});
 	return std::accumulate(crabs.begin(), crabs.end(), 0ULL, [&](const int& sum, const int& crab) {
-		return sum + std::abs(crab - static_cast<int64_t>(pivot));
+		return sum + std::abs(crab - static_cast<int64_t>(*pivot));
 	});
 }
 
 uint64_t Part2(const std::vector<int>& data) {
 	auto crabs = data;
-	std::sort(crabs.begin(), crabs.end());
-	int64_t pivot = crabs[crabs.size() / 2];
+	auto pivot = crabs.begin() + crabs.size() / 2;
+	std::nth_element(crabs.begin(), pivot, crabs.end(), std::less{});
 	int64_t avg = std::accumulate(crabs.begin(), crabs.end(), 0ULL) / crabs.size();
-	if (avg < pivot) { avg++; }
+	if (avg < *pivot) { avg++; }
 	return std::accumulate(crabs.begin(), crabs.end(), 0ULL, [&](const int& sum, const int& crab) {
 		int64_t d = std::abs(crab - avg);
 		return sum + ((d * (d + 1)) / 2);
