f470d55371e2298928d4fc61438aee342e0d26d4
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-08 22:10:23 +0100

    Day8: Part 2 wip
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day8.h
+++ Day/Day8.h
@@ -11,35 +11,6 @@
 namespace aoc {
 namespace Day8 {
 
-const std::array<std::string, 10> digits = {
-	("abcefg"),
-	("cf"),	
-	("acdeg"),	
-	("acdfg"),		
-	("bcdf"),		
-	("abdfg"),		
-	("abdefg"),
-	("acf"),
-	("abcdefg"),
-	("abcdfg")
-};
-
-//  dddd
-// e    a
-// e    a
-//  ffff
-// g    b
-// g    b
-//  cccc
-
-// a = d
-// b = e
-// c = a
-// d = f
-// e = g
-// f = b
-// g = c
-
 uint64_t Part1(const std::vector<std::string>& data) {
 	bool counting = false;
 
@@ -67,47 +38,41 @@ uint64_t Part1(const std::vector<std::string>& data) {
 	return total;
 }
 
+uint64_t decode(const std::array<char, 7> &positions, const std::string& segment) {
+	switch (segment.length()) {
+		case 2: return 1;
+		case 4: return 4;
+		case 3: return 7;
+		case 7: return 8;
+		default: break;
+	}
+
+	return 0;
+}
+
 uint64_t Part2(const std::vector<std::string>& data) {
-	bool counting = false;
-
-	for (auto& d : mapped_digits) {
-		std::sort(d.begin(), d.end());
-	}
-
-	auto find_index = [&](const std::string& signal){
-		auto it = std::find(mapped_digits.begin(), mapped_digits.end(), signal);
-		return std::distance(mapped_digits.begin(), it);
-	};
-
-	uint64_t total{0};
+	std::string token;
 	for (auto& line : data) {
-		std::vector<int> segment_order;
-		std::vector<int> segments;
 
-		std::string signal;
+		std::array<char, 7> positions;
+
+		auto connect = [&](char pos, char letter) { positions[letter - 'a'] = pos; };
+
+		std::vector<std::string> lhs, rhs;
 		std::stringstream iss(line);
-		std::string res;
+		for(; iss >> token; lhs.emplace_back(token)) { if(token == "|") break; }
+		for(; iss >> token; rhs.emplace_back(token));
 
-		while(iss >> signal) {
+		std::sort(lhs.begin(), lhs.end(), [](const auto& lhs, const auto& rhs){ return lhs.length() < rhs.length(); });
 
-			if (signal == "|") {
-				counting = true;
-				continue;
-			}
-			
-			if (counting) {
-				res += find_index(signal);
-				break;
-			} else {
-				std::sort(signal.begin(), signal.end());
-				segment_order.push_back(find_index(signal));	
-			}
-		}
+		const auto& one = lhs[0];
+		const auto& four = lhs[1];
+		const auto& seven = lhs[2];
 
-		counting = false;
+		
 	}
 
-	return total;
+	return 0;
 }
 
 }
