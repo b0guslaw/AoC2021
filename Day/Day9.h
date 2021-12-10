6f15e15c69d75be0081f4ef9723b823bbe76df76
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-10 11:56:11 +0100

    Day9: part 1 optimisation
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day9.h
+++ Day/Day9.h
@@ -8,28 +8,30 @@ namespace aoc {
 namespace Day9 {
 
 uint64_t Part1(std::vector<std::string>& data) {
-
-	std::vector<std::string> field;
-	field.emplace_back(std::string(data[0].size() + 2, '9'));
-	for (auto& line : data) {
-		line.insert(0, 1 ,'9');
-		line += '9';
-		field.push_back(line);
-	}
-	field.emplace_back(std::string(data[0].size(), '9'));
+	uint64_t total{0};
+	int width{static_cast<int>(data[0].length())};
+	int height{static_cast<int>(data.size())};
 
 	const auto lowest = [&](const int row, const int col) {
-	return field[row-1][col] > field[row][col] && field[row+1][col] > field[row][col] &&
-		   field[row][col+1] > field[row][col] && field[row][col-1] > field[row][col];
+		if (data[row][col] == '9') return false;
+		if (data[row][col] == '0') return true;
+		
+		bool lowest = true;
+		if ((row > 0 && data[row][col] >= data[row - 1][col]) ||
+		   	(col > 0 && data[row][col] >= data[row][col - 1]) ||
+		   	(row < height - 1 && data[row][col] >= data[row + 1][col]) ||
+		   	(col < width  - 1 && data[row][col] >= data[row][col+ 1])) {
+			   lowest = false;
+		   }
+		   return lowest;
 	};
 
-	int total = 0;
-	for (int row{1}; row < field.size() - 1; row++) {
-		for (int col{1}; col < field[row].length() - 1; col++) {
-			if (lowest(row, col)) {
-				total += 1 + (field[row][col] - '0');
-			}
+	for(int i{0}; i < width * height; ++i) {
+		int x = i % width, y = i / width;
+		if ((lowest(y, x))) {
+			total += data[y][x] - 47;
 		}
+		
 	}
 	return total;
 }
