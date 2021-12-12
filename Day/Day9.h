ff632c2f1500b71bcf0e0f00c77a0d977389cd8a
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-12 18:22:30 +0100

    Day9: Finish
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day9.h
+++ Day/Day9.h
@@ -3,42 +3,97 @@
 
 #include <vector>
 #include <iostream>
+#include <array>
+#include <algorithm>
 
 namespace aoc {
 namespace Day9 {
 
-uint64_t Part1(std::vector<std::string>& data) {
+uint64_t Part1(const std::vector<std::string>& data) {
 	uint64_t total{0};
-	int width{static_cast<int>(data[0].length())};
+	
+	int width{static_cast<int>(data.front().length())};
 	int height{static_cast<int>(data.size())};
 
-	const auto lowest = [&](const int row, const int col) {
-		if (data[row][col] == '9') return false;
-		if (data[row][col] == '0') return true;
-		
-		bool lowest = true;
-		if ((row > 0 && data[row][col] >= data[row - 1][col]) ||
-		   	(col > 0 && data[row][col] >= data[row][col - 1]) ||
-		   	(row < height - 1 && data[row][col] >= data[row + 1][col]) ||
-		   	(col < width  - 1 && data[row][col] >= data[row][col+ 1])) {
-			   lowest = false;
-		   }
-		   return lowest;
+	auto const is_valid = [&](unsigned int x, unsigned int y){
+		auto isvalid =  x <= height - 1 && y <= width - 1;
+		return isvalid;
 	};
 
-	for(int i{0}; i < width * height; ++i) {
-		int x = i % width, y = i / width;
-		if ((lowest(y, x))) {
-			total += data[y][x] - 47;
+	for (unsigned x{0}; x < height; x++) {
+		for (unsigned int y{0}; y < width; y++) {
+			
+			static constexpr char max{'9'};
+
+			if (data[x][y] == max) {
+				continue;
+			}
+
+			const auto neighbours = std::array<char, 4> {
+					is_valid(x - 1, y) ? data[x - 1][y] : max,
+					is_valid(x + 1, y) ? data[x + 1][y] : max,
+					is_valid(x, y + 1) ? data[x][y + 1] : max,
+					is_valid(x, y - 1) ? data[x][y - 1] : max
+			};
+
+			const auto minimum = std::all_of(neighbours.begin(), neighbours.end(), [&](const char neighbour) {
+				 return neighbour > data.at(x).at(y);;
+			});
+
+			total += static_cast<int>(data.at(x).at(y) - 47) * minimum;
 		}
-		
 	}
 	return total;
 }
 
+uint64_t flood_fill(std::vector<std::string>& field, unsigned x, unsigned int y) {
+	const int width{static_cast<int>(field.front().length()) - 1};
+	const int height{static_cast<int>(field.size()) - 1};
+
+	uint64_t size{1};
+	static constexpr char max{'9'};
+	field[x][y] = max;
+
+	static constexpr std::array<std::array<int, 2>, 4> directions = {{
+		{1,0},
+		{-1,0},
+		{0,1},
+		{0,-1}
+	}};
+
+	auto const is_valid = [&](unsigned int x, unsigned int y){
+		auto isvalid = x <= height && y <= width && field[x][y] < max;
+		return isvalid;
+	};
+
+	for (auto& dir : directions) {
+		if (is_valid(x + dir[0], y + dir[1])) {
+			size += flood_fill(field, x + dir[0], y + dir[1]);
+		}
+	}
+	
+	return size;
+}
+
 uint64_t Part2(const std::vector<std::string>& data) {
-	auto _m = data;
-	return 0;
+	std::vector<uint64_t> basin_sizes;
+	auto field = data;
+
+	int width{static_cast<int>(field.front().length())};
+	int height{static_cast<int>(field.size())};
+	
+	for(int x{0}; x < height; x++) {
+		for (int y{0}; y < width; y++) {
+			if (field[x][y] < '9') {
+				basin_sizes.push_back(
+					flood_fill(field, x, y)
+				);
+			}
+		}
+	}
+
+	std::sort(basin_sizes.begin(), basin_sizes.end(), std::greater<int>{});
+	return basin_sizes[0] * basin_sizes[1] * basin_sizes[2];
 }
 
 }
