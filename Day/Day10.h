2e4ee6cc93c2d6f88143b4464eca1d41675bf75d
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2021-12-15 09:11:58 +0100

    Day10: Complete
    Signed-off-by: Ralph Dworzanski <ralph.dworzanski@gmail.com>

--- Day/Day10.h
+++ Day/Day10.h
@@ -9,46 +9,60 @@
 namespace aoc {
 namespace Day10 {
 
+auto syntax_check(const std::string line) {
+    uint64_t part1_score{0}, part2_score{0};
+    std::map<char, int> scores = {{')',3}, {']',57}, {'}',1197}, {'>',25137}};
+    std::vector<char> stack;
+    for (auto& c : line) {
+        switch (c) {
+            case '(':
+            case '{':
+            case '[':
+            case '<':
+                stack.push_back(c);
+                continue;
+            case ')':
+                if (stack.back() != '(') { part1_score += scores[c]; }
+                break;
+            case '}':
+                if (stack.back() != '{') { part1_score += scores[c]; }
+                break;
+            case ']':
+                if (stack.back() != '[') { part1_score += scores[c]; }
+                break;
+            case '>':
+                if (stack.back() != '<') { part1_score += scores[c]; }
+                break;
+        }
+
+        stack.pop_back();
+    }
+    while(!stack.empty()) {
+        part2_score = part2_score * 5 + std::string("([{<").find(stack.back()) + 1;
+        stack.pop_back();
+    }
+    return std::make_pair(part1_score, part2_score);
+}
+
 uint64_t Part1(const std::vector<std::string>& data) {
     uint64_t sum{0};
-    const std::map<char, int> scores = {{')',3}, {']',57}, {'}',1197}, {'>',25137}};
-    const std::map<char, char> pairs = {{'(',')'}, {'[',']'}, {'{','}'}, {'<','>'}};
-    for (const auto& line : data) {
-        std::stack<char> stack;
-        for (auto& c : line) {
-            if(c == '(' || c == '[' || c == '{' || c == '<') {
-                stack.push(pairs.at(c));
-            } else {
-                if (c != stack.top()) {
-                    sum += scores.at(c);
-                    break;
-                }
-                stack.pop();
-            }
-        }
+    for (auto& line : data) {
+        auto [result, ignore] = syntax_check(line);
+        sum += result;
     }
     return sum;
 }
 
 uint64_t Part2(const std::vector<std::string>& data) {
-	uint64_t sum{0};
-    std::vector<std::string> incomplete;
-    const std::map<char, char> pairs = {{'(',')'}, {'[',']'}, {'{','}'}, {'<','>'}};
-    for (const auto& line : data) {
-        std::stack<char> stack;
-        for (auto& c : line) {
-            if(c == '(' || c == '[' || c == '{' || c == '<') {
-                stack.push(pairs.at(c));
-            } else {
-                if (c != stack.top()) {
-                    break;
-                }
-                stack.pop();
-            }
+    std::vector<uint64_t> sums;
+    for (auto& line : data) {
+        auto [ignore, result] = syntax_check(line);
+        if (ignore == 0) {
+            sums.push_back(result);
         }
-        incomplete.push_back(line);
     }
-    return sum;
+    std::sort(sums.begin(), sums.end());
+    return sums[sums.size() / 2];
 }
 }
 }
